b796ac2266c125712cadac75dff49a97fb07795c
leeem <heezy109@gmail.com>
2024-04-11 09:47:34 +0000

    prac6

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # main.py
 import argparse
-import prac1
+import prac1 # prac1.py를 불러옴
 import prac2
 import prac3
+import prac4_yerim
+import prac6_yerim
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3'], help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4_yerim', 'prac6_yerim'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -15,6 +17,10 @@ def main():
         prac2.sum_two_numbers()
     elif args.script == 'prac3':
         prac3.calculate_average()
+    elif args.script == 'prac4_yerim':
+        prac4_yerim.read_file()
+    elif args.script == 'prac6_yerim':
+        prac6_yerim.game_loop()   
 
 if __name__ == "__main__":
     main()
